30e9624541f761a37003db8a298bde9d60e6d175
Emilianoperez11 <elibradoperez@gmail.com>
2024-11-12 20:07:38 -0600

    problema13.py

--- problema13.py
+++ problema13.py
@@ -4,7 +4,4 @@ for i in range (10):
     numero = float(input("ingrese el numero "))
     suma += numero
 
-promedio = suma / 10
-
-print(f"la suma de los  numeros es:{suma:.2f}")
-print(f"el promedio de los  numeros es:{promedio:.2f}")
+    print(f"suma de los  valores es: {suma:.2f}")
