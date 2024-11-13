e3a36206638d7ed40a9d75968ef6d24f1b45a22d
Emilianoperez11 <elibradoperez@gmail.com>
2024-11-12 19:54:07 -0600

    problema9.py

---
+++ problema9.py
@@ -0,0 +1,10 @@
+numero = (float(input("ingrese el numero ")))
+
+if numero > 0 :
+    print("el numero es positivo")
+
+elif numero < 0 :
+    print("el numero es negativo")
+
+else:
+    print("el numero es igual a cero")
