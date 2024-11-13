0950c342731af5cf8a4eeabe11130d9265821406
Emilianoperez11 <elibradoperez@gmail.com>
2024-11-12 19:48:24 -0600

    problema1.py

---
+++ problema1.py
@@ -0,0 +1,6 @@
+primernumero = float(input("ingresa el primer numero:"))
+segundonumero = float(input("ingresa el segundo numeron:"))
+
+suma = primernumero + segundonumero
+
+print ( "la suma de los numeros es:",suma)
