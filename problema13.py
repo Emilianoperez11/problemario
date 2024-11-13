579e6787eaf77aa29d86013a7dd14464338105b0
Emilianoperez11 <elibradoperez@gmail.com>
2024-11-12 19:58:17 -0600

    problema13.py

---
+++ problema13.py
@@ -0,0 +1,10 @@
+suma = 0
+
+for i in range (10):
+    numero = float(input("ingrese el numero "))
+    suma += numero
+
+promedio = suma / 10
+
+print(f"la suma de los  numeros es:{suma:.2f}")
+print(f"el promedio de los  numeros es:{promedio:.2f}")
