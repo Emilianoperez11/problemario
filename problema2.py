ecd3ec9e7778660997fe798969bcc60c39d98764
Emilianoperez11 <elibradoperez@gmail.com>
2024-11-12 19:49:32 -0600

    problema2.py

---
+++ problema2.py
@@ -0,0 +1,7 @@
+primernumero = float(input("ingresa el primer numero"))
+segundonumero = float(input("ingresa el segundo numero"))
+tercernumero = float(input("ingresa el tercer numero"))
+
+promedio = primernumero + segundonumero + tercernumero / 3
+
+print("el promedio de los tres numeros es:",promedio)
