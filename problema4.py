8ebb0c56246fa2768241abc9acb2a30887f24cab
Emilianoperez11 <elibradoperez@gmail.com>
2024-11-12 19:50:44 -0600

    problema4.py

---
+++ problema4.py
@@ -0,0 +1,11 @@
+cal1 = float(input("ingresa la primera calificacion "))
+cal2 = float(input("ingresa la segunda calificacion"))
+cal3 = float (input("ingresa la tercera calificacion"))
+
+promedio = cal1 + cal2 + cal3 / 3
+
+if cal >=7 :
+    print("aprobo")
+
+else:
+    print("recuperar")
