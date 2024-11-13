1f18a60dd1298682ed2ed239cb1325984865450e
Emilianoperez11 <elibradoperez@gmail.com>
2024-11-12 19:54:44 -0600

    problema10.py

---
+++ problema10.py
@@ -0,0 +1,8 @@
+for i in  range(1,25):
+    print(f"/nAlumno {i}:")
+    apellido = input("ingrese el apellido del alumno")
+    nombre = input("ingrese el nombre del alumno")
+    nota = input("ingrese la nota del alumno")
+
+    print(f"/ndatos del alumno {i}:")
+    print(f"Nombre y Apellido y nota:{nombre},{apellido},{nota}")
